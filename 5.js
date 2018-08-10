e9d46e68e1df4555deb87a3b157ec63d9e3e0b69
nathias <n@th1.as>
2018-08-10 13:17:44 +0200

    hoisting of declarations

---
+++ 5.js
@@ -0,0 +1,68 @@
+//Code Snippet 1
+
+var counter = 5;
+var rate = 3;
+console.log('The total value is ' + String(counter * rate));
+
+function counter(count) {
+  // ...
+}
+
+// function counter(count) {
+//   // ...
+// }
+
+// var rate;
+
+// counter = 5;
+// rate = 3;
+
+// console.log('The total value is ' + String(counter * rate));
+// logs: The total value is 15
+
+//Code Snippet 2
+
+function counter(count) {
+  // ...
+}
+
+console.log('The total value is ' + String(counter * rate));
+
+var counter = 5;
+var rate = 3;
+
+// function counter(count) {
+//   // ...
+// }
+
+// var rate;
+
+// console.log('The total value is ' + String(counter * rate));
+// // logs: The total value is NaN
+
+// counter = 5;
+// rate = 3;
+
+//Code Snippet 3
+
+var counter = 5;
+var rate = 3;
+
+function counter(count) {
+  // ...
+}
+
+console.log('The total value is ' + String(counter * rate));
+
+// function counter(count) {
+//   // ...
+// }
+
+// var rate;
+
+// counter = 5;
+// rate = 3;
+
+// console.log('The total value is ' + String(counter * rate));
+// // logs: The total value is 15
+
